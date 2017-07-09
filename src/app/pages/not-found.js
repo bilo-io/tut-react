1113b9e12d7a66d72c7632a8122bbd482ba798f1
Bilo Lwabona <bilo.lwabona@gmail.com>
2017-07-09 22:14:05 +0200

    typo fix

--- src/app/pages/not-found.js
+++ src/app/pages/not-found.js
@@ -4,7 +4,7 @@ export default class NotFound extends React.Component {
     render() {
         return (
             <div>
-                <h1><b>404: NOTE Found</b></h1>
+                <h1><b>404 - NOT Found</b></h1>
                 <h2>The page can't be found</h2>
             </div>
         )
