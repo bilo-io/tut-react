0c4fb722990f3230b03503296dafef8b0f08dc23
Bilo <bilo.lwabona@gmail.com>
2017-07-11 16:54:34 +0200

    added ol map

---
+++ src/app/components/ol-map.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ol from 'openlayers';
+const OL = require('openlayers');
+require('./map.scss');
+
+export default class OLMap extends React.Component {
+    componentDidMount() {
+        this.initOL();
+    }
+    initOL() {
+        let overlays = new ol.layer.Group({
+            title: 'map-layer-group',
+            openInLayerSwitcher: true,
+            layers: [
+                new ol.layer.Tile({
+                    title: 'osmLayer',
+                    source: new ol.source.OSM()
+                })]
+        });
+        let ct = ol.proj.transform([18.4241, -33.9249], 'EPSG:4326', 'EPSG:3857');
+        let view = new ol.View({
+            center: ct,
+            zoom: 6
+        });
+        this.map = new ol.Map({
+            target: 'ol-map',
+            view: new ol.View({
+                center: ct,
+                zoom: 5,
+                minZoom: 0,
+                maxZoom: 9
+            }),
+            // controls: ol.control.defaults().extend([
+            //     new ol.control.LayerSwitcher()
+            // ]),
+            layers: [overlays]
+        });
+    }
+    render() {
+        return (
+            <div className="map-container" >
+                <div id="ol-map" className="map-component" />
+            </div >
+        )
+    }
+}
