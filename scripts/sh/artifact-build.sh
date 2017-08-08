aaeceb52f82efba401660307bac7145edc68e30c
Bilo Lwabona <bilo.lwabona@gmail.com>
2017-08-08 18:05:06 +0200

    deleted scripts folder

--- scripts/sh/artifact-build.sh
+++
@@ -1,8 +0,0 @@
-rm -rf artifact dist
-npm run build
-mkdir artifact
-mv dist ./artifact
-cp server.js ./artifact
-cp package.json ./artifact
-cd ./artifact
-npm install --production
--- scripts/sh/artifact-deploy.sh
+++
@@ -1,4 +0,0 @@
-mkdir ~/Prod_Deployments
-cp ./artifact ~/Prod_Depoloyments
-cd ~/Prod_Deployments
-node server.js
